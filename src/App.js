3eee323ab116763e0861982b5730968316f5c2d7
HAJIWEE <HAJIWEE94.11@gmail.com>
2022-11-19 12:01:29 +0800

    Add react query

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React from "react";
-import logo from "./logo.png";
+import {
+  QueryClient,
+  QueryClientProvider,
+  ReactQueryDevtools,
+} from "react-query";
 import "./App.css";
 import PostCard from "./Component/postCard";
+
+const queryClientConfig = {
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      refetchOnMount: "always",
+      refetchOnWindowFocus: "always",
+      refetchOnReconnect: "always",
+      cacheTime: 1000 * 30, //30 seconds
+      refetchInterval: 1000 * 30, //30 seconds
+      refetchIntervalInBackground: false,
+      suspense: false,
+      staleTime: 1000 * 30,
+    },
+    mutations: {
+      retry: 2,
+    },
+  },
+};
+
+const queryClient = new QueryClient(queryClientConfig);
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <PostCard />
-        </header>
+      // <QueryClientProvider client={queryClient} contextSharing={true}>
+      <div className="app">
+        <PostCard />
       </div>
+      /* <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider> */
     );
   }
 }
