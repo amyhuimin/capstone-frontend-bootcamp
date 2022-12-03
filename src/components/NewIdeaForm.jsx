8f01d5a4a9b3252d2dd6f4a5b9efebd6c3a693ba
amyhuimin <amylimhuimin@gmail.com>
2022-12-03 11:51:06 +0900

    updated create idea form

--- src/components/NewIdeaForm.jsx
+++ src/components/NewIdeaForm.jsx
@@ -6,6 +6,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import Box from "@mui/material/Box";
 
 export default function NewIdeaForm() {
   const [open, setOpen] = React.useState(false);
@@ -20,7 +21,6 @@ export default function NewIdeaForm() {
 
   return (
     <div>
-
       <Button
         onClick={handleClickOpen}
         sx={{ backgroundColor: "#FFCE35", color: "black" }}
@@ -30,26 +30,70 @@ export default function NewIdeaForm() {
       </Button>
 
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
+        <Box
+          className="overflow-auto"
+          sx={{ maxHeight: "100vh", width: "40vw", pt: "10%" }}
+        >
+          <DialogTitle>What's the idea? (details)</DialogTitle>
+          <DialogContent>
+            {/* <DialogContentText>
             To subscribe to this website, please enter your email address here.
             We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
-        </DialogActions>
+          </DialogContentText> */}
+            <TextField
+              autoFocus
+              id="outlined-basic"
+              variant="outlined"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              margin="dense"
+              fullWidth
+              type="ideaName"
+              placeholder="Idea Name"
+            />
+            <TextField
+              autoFocus
+              id="outlined-basic"
+              variant="outlined"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              fullWidth
+              type="shortDescription"
+              // label="Outlined"
+              // value={shortDescription}
+              // onChange={handleChange}
+              placeholder="One liner description for a mum's test!"
+            />
+            <TextField
+              autoFocus
+              id="outlined-basic"
+              variant="outlined"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              fullWidth
+              type="detailedDescription"
+              placeholder="Detailed description"
+            />
+            <TextField
+              autoFocus
+              id="outlined-basic"
+              variant="outlined"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              fullWidth
+              type="purpose"
+              placeholder="What's the purpose of the idea"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Save Idea!</Button>
+          </DialogActions>
+        </Box>
       </Dialog>
     </div>
   );
