e182ccb0236b368d898f2b1c5cca54e106b69053
HAJIWEE <HAJIWEE94.11@gmail.com>
2022-12-10 23:36:37 +0800

    Newsfeed Query Route done

--- src/Queries.js
+++ src/Queries.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
+const BackEndUrl = "http://localhost:4000";
+
 async function NewsQuery(props) {
   const newSourceURL = `https://newsapi.org/v2/top-headlines?country=${props}&category=business&category=technology&apiKey=fc313bcc2c334b46a7de0f9a08874d47`;
   const data = axios.get(newSourceURL);
   return data;
 }
 
-export { NewsQuery };
+async function PostsQuery() {
+  const data = axios.get(BackEndUrl + "/");
+  return data;
+}
+
+export { NewsQuery, PostsQuery };
--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 //React Query imports
-import { NewsQuery } from "../Queries.js";
+import { PostsQuery } from "../Queries.js";
 import { useQuery } from "@tanstack/react-query";
 
 import "./cssFiles/rightNewsBar.css";
@@ -13,41 +13,25 @@ import "./cssFiles/newsfeed.css";
 
 function NewsFeed() {
   //React Query hook
-  // const { isLoading, data, isError } = useQuery(
-  //   ["topheadlines", country], //[key(Ownself name it), props]
-  //   () => NewsQuery(country) //function you want to use from Queries.js
-  // );
-
-  // if (isLoading) {
-  //   return (
-  //     <div className="rightNewsBar">
-  //       <Box bgcolor="white">
-  //         <div>is Loading</div>
-  //       </Box>
-  //     </div>
-  //   );
-  // }
-  // if (isError) {
-  //   return (
-  //     <div className="rightNewsBar">
-  //       <Box bgcolor="white">
-  //         <div>Error Loading News Headlines</div>
-  //       </Box>
-  //     </div>
-  //   );
-  // }
-  // const { articles } = data.data;
-  return (
-    <div className="newsfeed">
-      <Box bgcolor="transparent">
-        <div className="newsfeedscroll">
-          {postData.map((posts) => {
-            return <PostCard key={postData.indexOf(posts)} content={posts} />;
-          })}
-        </div>
-      </Box>
-    </div>
+  const { isLoading, data, isError } = useQuery(
+    ["posts"], //[key(Ownself name it), props]
+    () => PostsQuery() //function you want to use from Queries.js
   );
+
+  if (!isLoading && !isError) {
+    console.log(data);
+    return (
+      <div className="newsfeed">
+        <Box bgcolor="transparent">
+          <div className="newsfeedscroll">
+            {data.data.map((posts) => {
+              return <PostCard key={postData.indexOf(posts)} content={posts} />;
+            })}
+          </div>
+        </Box>
+      </div>
+    );
+  }
 }
 
 export default NewsFeed;
